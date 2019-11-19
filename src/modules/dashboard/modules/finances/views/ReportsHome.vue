175a2b8b418cf48faad637468ab0f2d84df6501a
guibolt <guilhermedosreiss@gmail.com>
2019-11-19 17:45:53 -0300

    finished

---
+++ src/modules/dashboard/modules/finances/views/ReportsHome.vue
@@ -0,0 +1,157 @@
+<template>
+  <v-layout
+    row
+    wrap
+  >
+
+    <v-flex xs12>
+      <ToolbarByMonth
+        format="MM-YYYY"
+        color="color"
+        :month="month || $route.query.month"
+        @month="changeMonth"
+        
+      />
+    </v-flex>
+
+    <v-flex
+      v-for="chart in charts"
+      :key="chart.title"
+      xs12
+      sm6
+      md6
+      lg6
+      xl6
+    >
+      <v-card>
+        <v-card-text>
+          <h2 class="font-weight-light mb-4">{{ chart.title }}</h2>
+          <canvas :ref="chart.refId"></canvas>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+
+  </v-layout>
+</template>
+
+<script>
+
+import Chart from 'chart.js'
+import { mapActions, mapState } from 'vuex'
+import { Subject } from 'rxjs'
+import { mergeMap } from 'rxjs/operators'
+
+import { generateChartConfigs } from '@/utils'
+import RecordsService from './../services/records-service'
+import ToolbarByMonth from './../components/ToolbarByMonth.vue'
+
+export default {
+  name: 'ReportsHome',
+  components: {
+    ToolbarByMonth
+  },
+  data: () => ({
+    chartIncomesExpenses: undefined,
+    chartCategoryExpenses: undefined,
+    charts: [
+      { title: 'Receitas vs Despesas', refId: 'chartIncomesExpenses' },
+      { title: 'Despesas por Categoria', refId: 'chartCategoryExpenses' }
+    ],
+    monthSubject$: new Subject(),
+    records: [],
+    subscriptions: []
+  }),
+  computed: {
+    ...mapState('finances', ['month']),
+    recordsSum(){
+      return this.records.reduce((acc,record) => acc + record.amount,0)
+    },
+    color () {
+      return this.recordsSum < 0 ? 'error' : 'primary'
+    }
+  },
+  created () {
+    this.setTitle({ title: 'Relatórios' })
+    this.setRecords()
+  },
+  destroyed () {
+    this.subscriptions.forEach(s => s.unsubscribe())
+  },
+  methods: {
+    ...mapActions(['setTitle']),
+    ...mapActions('finances', ['setMonth']),
+    changeMonth (month) {
+      this.$router.push({
+        path: this.$route.path,
+        query: { month }
+      })
+      this.setMonth({ month })
+      this.monthSubject$.next(month)
+    },
+    createChart (chartId, options) {
+      const ref = Array.isArray(this.$refs[chartId])
+        ? this.$refs[chartId][0]
+        : this.$refs[chartId]
+      const ctx = ref.getContext('2d')
+      return new Chart(ctx, options)
+    },
+    setCharts () {
+      // receitas e despesas
+      const chartIncomesExpensesConfigs = generateChartConfigs({
+        type: 'bar',
+        items: this.records,
+        keyToGroup: 'type',
+        keyOfValue: 'amount',
+        aliases: { CREDIT: 'Receitas', DEBIT: 'Despesas' },
+        backgroundColors: [
+           this.$vuetify.theme.primary,
+          this.$vuetify.theme.error
+        ]
+      })
+
+      if (this.chartIncomesExpenses) {
+        this.chartIncomesExpenses.data.datasets = chartIncomesExpensesConfigs.data.datasets
+        this.chartIncomesExpenses.update()
+      } else {
+        this.chartIncomesExpenses =
+          this.createChart('chartIncomesExpenses', chartIncomesExpensesConfigs)
+      }
+
+      // despesas por categoria
+      // chartCategoryExpenses
+      const chartCategoryExpensesConfigs = generateChartConfigs({
+        type: 'doughnut',
+        items: this.records.filter(r => r.type === 'DEBIT'),
+        keyToGroup: 'category.description',
+        keyOfValue: 'amount',
+        backgroundColors: [
+          this.$vuetify.theme.accent,
+          this.$vuetify.theme.warning,
+          this.$vuetify.theme.info,
+          this.$vuetify.theme.success
+        ]
+      })
+
+      if (this.chartCategoryExpenses) {
+        this.chartCategoryExpenses.data.datasets = chartCategoryExpensesConfigs.data.datasets
+        this.chartCategoryExpenses.data.labels = chartCategoryExpensesConfigs.data.labels
+        this.chartCategoryExpenses.update()
+      } else {
+        this.chartCategoryExpenses =
+          this.createChart('chartCategoryExpenses', chartCategoryExpensesConfigs)
+      }
+    },
+    setRecords () {
+      this.subscriptions.push(
+        this.monthSubject$
+          .pipe(
+            mergeMap(month => RecordsService.records({ month }))
+          ).subscribe(records => {
+            this.records = records
+            this.setCharts()
+          })
+      )
+    }
+  }
+}
+</script>
---
+++ src/utils.js
@@ -0,0 +1,145 @@
+const errorHandler = (err, vm, info) => {
+  console.log('Vue [error handler]: ', err, info)
+  const jwtErrors = ['jwt malformed', 'jwt expired', 'jwt not active', 'invalid token']
+  if (jwtErrors.some(jwtError => err.message.includes(jwtError))) {
+    vm.$router.push({
+      path: '/login',
+      query: { redirect: vm.$route.path }
+    })
+  }
+}
+
+const formatError = message => {
+  const messageSplit = message.split(':')
+  return messageSplit[messageSplit.length - 1].trim()
+}
+
+const groupBy = (array, key, makeCurrentKey) => {
+  return array.reduce((accumulated, item) => {
+    const currentKey = makeCurrentKey(item, key)
+    return {
+      ...accumulated,
+      [currentKey]: [
+        ...(accumulated[currentKey] || []),
+        item
+      ]
+    }
+  }, {})
+}
+
+const idx = (object, keyPath) => {
+  const keys = keyPath.split('.')
+  return keys.reduce(
+    (obj, current) => (obj && obj[current] !== undefined) ? obj[current] : null, object
+  )
+}
+
+const generateChartOptions = (type) => {
+  let tooltips = {}
+  switch (type) {
+    case 'bar':
+      tooltips = {
+        callbacks: {
+          title () { },
+          label (tooltip, data) {
+            return data.datasets[tooltip.datasetIndex].label
+          }
+        }
+      }
+      break
+
+    case 'doughnut':
+      tooltips = {
+        callbacks: {
+          label (tooltip, data) {
+            const label = data.labels[tooltip.index]
+            const value = currencyFormatter()
+              .format(data.datasets[tooltip.datasetIndex].data[tooltip.index])
+            return `${label}: ${value}`
+          }
+        }
+      }
+      break
+  }
+
+  const scales = {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  }
+
+  return {
+    scales,
+    tooltips
+  }
+}
+
+const generateChartData = ({ items, keyToGroup, keyOfValue, aliases, type, backgroundColors }) => {
+  const grouped = groupBy(items, keyToGroup, idx)
+  const response = {}
+
+  for (let key in grouped) {
+    response[(aliases && aliases[key]) || key] =
+      grouped[key].reduce((acc, item) => acc + item[keyOfValue], 0)
+  }
+
+  const labels = Object.keys(response)
+
+  switch (type) {
+    case 'bar':
+      return {
+        datasets: labels.map((label, index) => ({
+          label: `${label}: ${currencyFormatter().format(response[label])}`,
+          data: [response[label] >= 0 ? response[label] : -response[label]],
+          backgroundColor: backgroundColors[index],
+          borderWidth: 0
+        }))
+      }
+
+    case 'doughnut':
+      return {
+        datasets: [{
+          data: labels.map(label => response[label] >= 0 ? response[label] : -response[label]),
+          backgroundColor: backgroundColors,
+          borderWidth: 0
+        }],
+        labels: items.length > 0 ? labels : []
+      }
+  }
+}
+
+const generateChartConfigs = (opts) => {
+  const { type } = opts
+  const data = generateChartData(opts)
+  const options = generateChartOptions(type)
+
+  return {
+    type,
+    data,
+    options
+  }
+}
+
+const currencyFormatter = ({ locale, currency } = { locale: 'pt-BR', currency: 'BRL' }) => {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency
+  })
+}
+
+const registerVuexModule = (rootStore, moduleName, store) => {
+  if (!(moduleName in rootStore._modules.root._children)) {
+    rootStore.registerModule(moduleName, store)
+  }
+}
+
+export {
+  currencyFormatter,
+  generateChartConfigs,
+  groupBy,
+  errorHandler,
+  formatError,
+  registerVuexModule
+}
